62ef7f9801c04b6f3fa66c3d8fac38b0551d8850
snacksncode <snacksncode@gmail.com>
2023-03-29 21:51:20 +0200

    cache images

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "*.png"],
       manifest: {
         name: "Notes PWA",
         short_name: "NotesPWA",
